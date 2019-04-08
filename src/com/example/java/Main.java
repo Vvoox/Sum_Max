7c17d400f7edfc6a5f4b5cbba69abba9f8f188ef
Khalil Daoulat <48126333+Vvoox@users.noreply.github.com>
2019-04-08 11:04:40 +0100

    Add files via upload

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -1,118 +1,47 @@
 package com.example.java;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    public static int size1;
-    public static int size2;
-    public static int[] somme;
+        public static long somme_int;
+        public static String somme_str;
+        public static long somme;
+        public static long max = 4194967296L;
 
-    public static void main (String[] args) {
-
-        System.out.println("Sum of 2 tables , you should enter size of table 1 and it values , and also for table 2");
-        System.out.print("The size of table 1 : ");
-        Scanner sz1 = new Scanner(System.in);
-         size1 = sz1.nextInt();
-        System.out.print("The size of table 2 : ");
-        Scanner sz2 = new Scanner(System.in);
-         size2 = sz1.nextInt();
-        int tab1[] = new int[size1];
-        int tab2[] = new int[size2];
-        int cn1 =1 , cn2=1;
-
-        for (int i =0 ; i<size1 ; i++){
-
-            System.out.print("Table 1 : value number "+cn1+" is : ");
-            Scanner vl=new Scanner(System.in);
-            tab1[i]=vl.nextInt();
-            cn1++;
+        public Main() {
         }
-        for (int j=0 ; j<size2 ; j++) {
 
-            System.out.print("Table 2 : value number  " + cn2 + " is : ");
-            Scanner vl = new Scanner(System.in);
-            tab2[j] = vl.nextInt();
-            cn2++;
-        }
-        System.out.println("The sum of max 2 number in tab1&tab2 : "+ ajouter(tab1,tab2));
-        ajouter1(tab1,tab2);
-        System.out.print("The sum of each single case with else in other table is : ");
-        for (int i=0 ; i<somme.length ; i++){
-            System.out.print(somme[i]+" ");
-        }
-    }
+        public static long somme(String a, String b) {
+            Long a1 = Long.parseLong(a);
+            Long b1 = Long.parseLong(b);
 
-   public static int ajouter(int[] tab1 , int[] tab2){
-       int max1=0 , max2=0;
+            if (a1 <= max && b1 <= max) {
 
-        for(int i =0 ; i<size1; i++){
-
-            for(int j =0 ; j<size2 ; j++){
-
-                if(tab1[i] >= tab1[j] && i!=j){
-
-                    if(max1<tab1[i]){ max1=tab1[i];}
-
-                    else break;
-
-                }
-                else {
-
-                    if(max1<tab1[j]){ max1=tab1[j]; }
-
-                    else break;
+                somme = a1 + b1;
             }
-        }
-       for( i=0 ; i<size2 ; i++) {
-
-           for (int j = 0; j < size2; j++) {
-
-               if (tab2[i] >= tab2[j] && i != j) {
-
-                   if (max2 < tab2[i]) {
-                       max2 = tab2[i];
-                   } else break;
-
-               } else {
-                   if (max2 < tab2[j]) {
-                       max2 = tab2[j];
-                   } else break;
-               }
-
-           }
-       }
-
-       }
-       int somme= max1+max2;
-
-       return somme;
-    }
-
-    public static int[] ajouter1(int[] tab1 , int[] tab2){
-
-
-        if(size1>=size2){
-            somme = new int[size1];
-            for(int i=0 ; i<size1 ; i++){
-                somme[i]=tab1[i]+tab2[i];
-               // System.out.print(somme[i]+"\t");
-
+            else {
+                a = a + "L";
+                b = b + "L";
+                a1 = Long.parseLong(a);
+                b1 = Long.parseLong(b);
+                somme = a1 + b1;
             }
+
+            return somme;
         }
-        
-        else {
-            somme = new int[size2];
-            for(int i=0 ; i<size2 ; i++){
-                somme[i]=tab1[i]+tab2[i];
-                //System.out.print(somme[i]+"\t");
 
-            }
+        public static void main(String[] args) {
+
+            System.out.print("Enter a value : ");
+            Scanner aa = new Scanner(System.in);
+            String a = aa.nextLine();
+
+            System.out.print("Enter b value : ");
+            Scanner bb = new Scanner(System.in);
+            String b = bb.nextLine();
+            System.out.print("a + b value is : " + somme(a, b));
         }
-        return somme;
-
-
-
-
-    }
 }
+
+
 
